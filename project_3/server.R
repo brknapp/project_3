8641591477e832f151a9dadf786733e20ba198b9
Bridget Knapp <brknapp@ncsu.edu>
2022-07-28 00:31:30 -0400

    12_31_7_28_2022

--- project_3/server.R
+++ project_3/server.R
@@ -1,3 +1,4 @@
+
 library(httr) #this package will help use use the URL we built to get information from the OMDb API
 library(jsonlite) #this package will help us convert the data we get from the OMDb API to a more usable format
 library(tidyverse) #this package will help us work with our nicely formatted data.
@@ -121,6 +122,8 @@ format_data <- function(data){
 }
 
 data <- format_data(data)
+
+data <- data[complete.cases(data), ]
 
 A <- data %>% select(-c(Title,Director,Writer,Actors ))
 
@@ -278,6 +281,36 @@ shinyServer(function(input,output)({
     selectInput("variabley", "select the Y variable", choices=names(data()))
   })
   
+  #Creates a new reactive values
+  # observe({
+  #   vals <- reactiveValues(intrain = createDataPartition(data$average_rating, p= input$split, list = FALSE),
+  #                          training = data[intrain,],
+  #                          testing = data[-intrain,])
+  # })
+  
+  
+  
+  
+  #   set.seed = set.seed(dim(data)[1]))
+  # 
+  # output$split_data <- renderPrint({
+  #   vals$set.seed
+  #   data <- data %>% select(input$response)
+  #   intrain <- createDataPartition(data, p= input$split, list = FALSE)
+  #   training <- data[intrain,]
+  #   testing <- data[-intrain,]
+  # })
+  
+  # output$split_data <- renderPrint({
+  # set.seed(dim(data)[1])
+  # train <- sample(1:nrow(data), size = nrow(data)*0.8)
+  # test <- dplyr::setdiff(1:nrow(data), train)
+  # Train <- data[train, ]
+  # Test <- data[test, ]
+  # 
+  # })
+  
+  
   # to output the structure of the dataset
   
   
@@ -285,12 +318,335 @@ shinyServer(function(input,output)({
   
   
   # For plot
-  # output$plot <- renderPlot({
-  #   ggplot(data(),aes_string(x=input$variablex, y=input$variabley)) +
-  #     geom_point() 
-  # }) 
+  output$plot <- renderPlot({
+    ggplot(data(),aes_string(x=input$variablex, y=input$variabley)) +
+      geom_point()
+  })
   
 }))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# library(httr) #this package will help use use the URL we built to get information from the OMDb API
+# library(jsonlite) #this package will help us convert the data we get from the OMDb API to a more usable format
+# library(tidyverse) #this package will help us work with our nicely formatted data.
+# library(lubridate) #this package will help us create dates 
+# library(ggplot2) #this package will help us make graphs
+# library(readr)
+# library(class)
+# library(caret)
+# library(DT)
+# library(tree)
+# library(readxl)
+# library(readr)
+# library(rpart.plot)
+# 
+# data<-read_csv("C://Users//Bridget//OneDrive//R_Scripts//repos//project_3//project_3//movie_data.csv") #put the excel sheet in the same file as the server.R and ui.R files!
+# 
+# #this will help us convert the Ratings.Value column to numeric
+# parse_number <- function(S){
+#   if(grepl("/", S)){
+#     A<-str_split(S, "/")
+#     A<-as.numeric(unlist(A))
+#     A<-A[[1]]/A[[2]]
+#     A<-A*100
+#   } else {
+#     A<-as.numeric(gsub("%","",S))
+#   }
+#   return(A)
+# }
+# 
+# #this will help us make the Summary_Awards column:
+# award <- function(S){
+#   if(is.na(S)){
+#     A<-"none"
+#     return(A)
+#   }
+#   S=tolower(S)
+#   #won and nominated:
+#   if((grepl("won", S) | grepl("win", S)) && (grepl("nomina", S))){
+#     A<-"won and nominated"
+#   }
+#   #only won:
+#   else if (grepl("won", S) | grepl("win", S)){
+#     A<-"won"
+#   }
+#   #only nominated:
+#   else if (grepl("nomina", S)){
+#     A<-"nomination"
+#   }
+#   #no awards or nominations:
+#   else {(A<-"none")
+#     return(A)
+#   }
+# }
+# 
+# mat1=NULL
+# format_data <- function(data){
+#   data$nchar_Title <- nchar(data$Title)
+#   data$Year <- as.numeric(data$Year)
+#   data$Rated <- as.factor(data$Rated)
+#   data$Released <- NULL
+#   data$Runtime <- as.numeric(gsub(" min","",data$Runtime))
+#   
+#   df <- data.frame(x = data$Genre)
+#   A<-df %>% separate(x, c('Genre_1'))
+#   data$first_Genre <- as.factor(A$Genre_1)
+#   data$Genre = NULL
+#   
+#   df <- data.frame(x = data$Country)
+#   A<-df %>% separate(x, c('Country_1'),sep=",")
+#   data$first_Country <- A$Country_1
+#   data$first_Country <- gsub("USA","United States",data$first_Country)
+#   data$first_Country <- as.factor(data$first_Country)
+#   data$Country = NULL
+#   
+#   data$Ratings.Value <- sapply(data$Ratings.Value, FUN=parse_number)
+#   data$Summary_Awards <- as.factor(sapply(data$Awards, FUN=award))
+#   data$Metascore <- as.numeric(data$Metascore)
+#   data$imdbRating <- as.numeric(data$imdbRating)*10
+#   data$imdbVotes <- as.numeric(gsub(",","",data$imdbVotes))
+#   data$DVD <- NULL
+#   data$BoxOffice <- gsub("\\$","",data$BoxOffice)
+#   data$BoxOffice <- as.numeric(gsub(",","",data$BoxOffice))
+#   
+#   data$Plot <- NULL
+#   data$Language <- NULL
+#   data$Awards<-NULL
+#   data$Poster<-NULL
+#   data$imdbID<-NULL
+#   data$Type<-NULL
+#   data$Production<-NULL
+#   data$Website<-NULL
+#   data$Response<-NULL
+#   
+#   movie_list<-unique(data$Title)
+#   
+#   for (i in movie_list){
+#     temp=data[is.element(data$Title,i),]
+#     Ratings.Value_mean<-mean(temp$Ratings.Value)
+#     Metascore<-unique(temp$Metascore)
+#     imdbRating<-unique(temp$imdbRating)
+#     
+#     #note: the average_rating column is the average of ratings from  
+#     #Internet Movie Database (when applicable)
+#     #Metacritic (when applicable)
+#     #Rotten Tomatoes (when applicable)
+#     #Metascore (when applicable)
+#     #imdbRating
+#     
+#     if(is.na(Metascore)==TRUE){
+#       temp$average_rating=(Ratings.Value_mean+imdbRating)/2
+#     }
+#     if(is.na(Metascore)==FALSE){
+#       temp$average_rating=(Ratings.Value_mean+Metascore+imdbRating)/3
+#     }
+#     mat1=rbind(mat1,temp)
+#   }
+#   mat1$Ratings.Value <- NULL
+#   mat1$Ratings.Source <- NULL
+#   mat1 <- unique(mat1)
+#   return(mat1)
+# }
+# 
+# data <- format_data(data)
+# 
+# A <- data %>% select(-c(Title,Director,Writer,Actors ))
+# 
+# A <- A[complete.cases(A), ]
+# 
+# set.seed(dim(A)[1])
+# train <- sample(1:nrow(A), size = nrow(A)*0.8)
+# test <- dplyr::setdiff(1:nrow(A), train)
+# ATrain <- A[train, ]
+# ATest <- A[test, ]
+# 
+# 
+# # trctrl <- trainControl(method = "repeatedcv", number = 10, repeats = 3)
+# # dtree_fit <- train(average_rating ~ ., data = ATrain, method = "rpart",
+# #                    trControl = trctrl,
+# #                    tuneGrid = data.frame(cp=seq(0,0.1,0.01)))
+# # pred <- predict(dtree_fit, newdata = dplyr::select(ATest, -average_rating))
+# #sqrt(mean((pred-ATest$average_rating)^2))
+# 
+# 
+# library(shiny)
+# library(caret)
+# library(tidyverse)
+# library(DT)
+# data("GermanCredit")
+# library(shiny)
+# library(ggplot2)
+# data<-mtcars
+# shinyServer(function(input,output)({
+#   
+#   ## Get the value of the dataset that is selected by user from the list of datasets
+#   ## to output the dataset
+#   output$dat <- renderPrint({
+#     ATrain
+#   })
+#   
+#   output$struct <- renderPrint({
+#     str(ATrain)
+#   })
+#   
+#   output$cont_table <- renderPrint({
+#     
+#     tv <- ATrain %>% select(input$table_variable,input$table_variable2)
+#     table(tv)
+#   })
+#   
+#   output$summary_table <- renderPrint({
+#     if(input$covariance=="None"){
+#       sv <- ATrain %>% select(input$summary_variable)
+#       summary(sv)
+#     } else if(input$covariance!="None"){
+#       A<-ATrain %>% select(input$summary_variable)
+#       B<-ATrain %>% select(input$covariance)
+#       cov(A,B)
+#       
+#     }
+#     
+#   })
+#   
+#   #if (x%%2 == 0 || x%%5 == 0) {
+#   
+#   # if(input$table_variable=="Rated"||input$table_variable=="first_Genre"||input$table_variable=="first_Country"||input$table_variable=="Summary_Awards"){
+#   #   tv <- ATrain %>% select(input$table_variable)
+#   #   table(tv)
+#   # } else if(input$table_variable2=="Rated"||input$table_variable2=="first_Genre"||input$table_variable2=="first_Country"||input$table_variable2=="Summary_Awards"){
+#   #   tv <- ATrain %>% select(input$table_variable,input$table)
+#   #   table(tv)
+#   #   
+#   # }
+#   
+#   
+#   
+#   
+#   # output$radio_button <- renderPlot({
+#   #   
+#   #   if(input$radio_button=="Just Classification") {
+#   #     g <- ggplot(GermanCredit, aes(x = Class))
+#   #     g + geom_histogram(stat="count")
+#   #   } else if(input$radio_button=="Classification and Unemployed"){
+#   #     g <- ggplot(GermanCredit, aes(x = Class))
+#   #     g + geom_histogram(stat="count",position="dodge",aes(fill=factor(EmploymentDuration.Unemployed))) +
+#   #       scale_fill_discrete(name="Unemployment status",labels=c("Employed", "Unemployed"))
+#   #   } else if(input$radio_button=="Classification and Foreign"){
+#   #     g <- ggplot(GermanCredit, aes(x = Class))
+#   #     g + geom_histogram(stat="count",position="dodge",aes(fill=factor(ForeignWorker))) +
+#   #       scale_fill_discrete(name="Status",labels=c("German", "Foreign"))
+#   #   }
+#   # })
+#   # 
+#   
+#   
+#   output$bar_plot <- renderPlot({
+#     ggplot(ATrain,aes_string(x=input$bar_plot_variable)) +
+#       geom_bar(fill="lightblue") +
+#       ggtitle(paste0("Bar Plot: ",input$bar_plot_variable ))
+#   })
+#   
+#   output$scatter_plot <- renderPlot({
+#     ggplot(ATrain,aes_string(x=input$scatter_plot_x_variable,y=input$scatter_plot_y_variable,color=input$color_variable)) +
+#       geom_point() +
+#       ggtitle(paste0("Scatter Plot: ",input$scatter_plot_x_variable, " vs.", input$scatter_plot_y_variable)) +
+#       scale_color_discrete(name = input$color_variable)
+#   })
+#   # output$cont_table <- renderPrint({
+#   #   if(input$table=="One Way") {
+#   #     tv1 <- ATrain %>% select(Rated)
+#   #     table(tv1)
+#   #     
+#   #     tv2 <- ATrain %>% select(first_Genre)
+#   #     table(tv2)
+#   #     
+#   #     tv3 <- ATrain %>% select(first_Country)
+#   #     table(tv3)
+#   #     
+#   #     tv4 <- ATrain %>% select(Summary_Awards)
+#   #     table(tv4)
+#   #   } #else if(input$radio_button=="Classification and Unemployed"){
+#   #   g <- ggplot(GermanCredit, aes(x = Class))
+#   #   g + geom_histogram(stat="count",position="dodge",aes(fill=factor(EmploymentDuration.Unemployed))) +
+#   #     scale_fill_discrete(name="Unemployment status",labels=c("Employed", "Unemployed"))
+#   # } else if(input$radio_button=="Classification and Foreign"){
+#   #   g <- ggplot(GermanCredit, aes(x = Class))
+#   #   g + geom_histogram(stat="count",position="dodge",aes(fill=factor(ForeignWorker))) +
+#   #     scale_fill_discrete(name="Status",labels=c("German", "Foreign"))
+#   # }
+#   #})
+#   
+#   
+#   # output$summary <- renderPlot({
+#   #   ggplot(ATrain, aes(x = average_rating)) +
+#   #   geom_histogram(stat="count")
+#   #   #summary(get(input$dataset))
+#   # })
+#   
+#   output$summary <- renderPlot({
+#     ggplot(ATrain,aes_string(x=input$hist_graph_variable)) +
+#       geom_histogram(color = "blue", fill = "red", size = 2) +
+#       ggtitle(paste0("Histogram: Distribution of ",input$hist_graph_variable ))
+#   })
+#   
+#   # output$radio_button <- renderPlot({
+#   #        if(input$select_box_1=="Bar Plot") {
+#   #          g <- ggplot(data(), aes_string(x = input$sel_bar_plot))
+#   #          g + geom_histogram(stat="count")
+#   #        }
+#   
+#   
+#   # Pulling the list of variable for choice of variable x
+#   output$varx <- renderUI({
+#     selectInput("variablex", "select the X variable", choices=names(data()))
+#   })
+#   
+#   # Pulling the list of variable for choice of variable y
+#   output$vary <- renderUI({
+#     selectInput("variabley", "select the Y variable", choices=names(data()))
+#   })
+#   
+#   # to output the structure of the dataset
+#   
+#   
+#   # for summary
+#   
+#   
+#   # For plot
+#   # output$plot <- renderPlot({
+#   #   ggplot(data(),aes_string(x=input$variablex, y=input$variabley)) +
+#   #     geom_point() 
+#   # }) 
+#   
+# }))
 
 
 
